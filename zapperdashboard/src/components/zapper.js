97e963f10aa88a69caf12ff7b37db369e40969e5
J Ben Anderson <revovisions@gmail.com>
2021-12-01 20:28:40 -0500

    zapper component changes

--- zapperdashboard/src/components/zapper.js
+++ zapperdashboard/src/components/zapper.js
@@ -13,6 +13,7 @@ import {
   Container,
 } from "reactstrap";
 
+//API key is standard Public key no secret needed
 const url =
   "https://api.zapper.fi/v1/apps?api_key=96e0cc51-a62e-42ca-acee-910ea7d2a241";
 
