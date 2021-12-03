6967ae990401f4a0b9ec8765beed1627dd499425
J Ben Anderson <revovisions@gmail.com>
2021-12-03 13:12:25 -0500

    api request fix with axios in zapper component rewritten and now working

--- zapperdashboard/src/components/zapper.js
+++ zapperdashboard/src/components/zapper.js
@@ -1,31 +1,45 @@
 import React from "react";
-import { useState } from "react";
+
 import axios from "axios";
 
-import {
-  Card,
-  CardBody,
-  CardImg,
-  CardText,
-  CardTitle,
-  Row,
-  Col,
-  Container,
-} from "reactstrap";
+export default class zapper extends React.Component {
+  state = {
+    defiList: [],
+  };
 
-//API key is standard Public key no secret needed
-const url =
-  "https://api.zapper.fi/v1/apps?api_key=96e0cc51-a62e-42ca-acee-910ea7d2a241";
-
-function Zapper() {
-  const [list, getList] = useState("");
-  axios
-    .get(`${url}past`)
-    .then((response) => {
-      const allList = response.data.list.allList;
-    })
-    .catch((error) => console.error(`Error: ${error}`));
-  return <></>;
+  componentDidMount() {
+    axios
+      .get(
+        `https://api.zapper.fi/v1/apps?api_key=96e0cc51-a62e-42ca-acee-910ea7d2a241`
+      )
+      .then((res) => {
+        const defiList = res.data;
+        this.setState({ defiList });
+      })
+      .catch((error) => console.error(`Error: ${error}`));
+  }
+  render() {
+    return (
+      <ul>
+        {this.state.defiList.map((defiList) => (
+          <li key={defiList.id}>{defiList.name}</li>
+        ))}
+      </ul>
+    );
+  }
 }
 
-export default Zapper;
+//API key is standard Public key no secret needed
+// const url =
+//   "https://api.zapper.fi/v1/apps?api_key=96e0cc51-a62e-42ca-acee-910ea7d2a241";
+
+// function Zapper() {
+//   const [list, getList] = useState("");
+//   axios
+//     .get(`${url}past`)
+//     .then((response) => {
+//       const allList = response.data.list.allList;
+//     })
+//     .catch((error) => console.error(`Error: ${error}`));
+//   return <></>;
+// }
