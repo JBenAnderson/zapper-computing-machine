9883c432ddf213d49acab00d83a91c58b5e3ee53
J Ben Anderson <revovisions@gmail.com>
2021-12-04 14:13:02 -0500

    added powered by Zapper SVG and link

--- zapperdashboard/src/App.js
+++ zapperdashboard/src/App.js
@@ -1,10 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Zapper from "../src/components/zapper.js";
+import mySVG from "../src/media/powered_by_zap_black.svg";
 
 function App() {
   return (
     <div className="App">
+      <div className="poweredBy">
+        <a href="https://zapper.fi">
+          <img src={mySVG} alt="powered by zapper.fi!" />
+        </a>
+      </div>
+
       <Zapper />
     </div>
   );
